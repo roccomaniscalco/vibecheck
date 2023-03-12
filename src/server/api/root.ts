dc5da68c7820b77d020f33f868d763155c4538b9
Rocco Maniscalco <rmaniscalco@magmutual.com>
2023-03-12 12:48:48 -0400

    remove boilerplate code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { createTRPCRouter } from "@/server/api/trpc";
-import { exampleRouter } from "@/server/api/routers/example";
-import {router} from "@/server/api/routers/router"
+import { router } from "@/server/api/routers/router";
 
 /**
  * This is the primary router for your server.
@@ -8,7 +7,6 @@ import {router} from "@/server/api/routers/router"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   router,
 });
 
