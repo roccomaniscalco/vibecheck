12ffa2a3b49139b4e183dbc28e01406a2c5ed3f8
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-03-27 00:37:07 -0400

    limit `commitSchema` to what is actually used

--- src/server/api/routers/router.ts
+++ src/server/api/routers/router.ts
@@ -3,76 +3,18 @@ import Sentiment from "sentiment";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-// TODO: limit schemas to the properties that are actually used
 const commitSchema = z.object({
-  url: z.string(),
   sha: z.string(),
-  node_id: z.string(),
   html_url: z.string(),
-  comments_url: z.string(),
   commit: z.object({
     url: z.string(),
-    author: z.object({
-      name: z.string(),
-      email: z.string(),
-      date: z.string(),
-    }),
     committer: z.object({
       name: z.string(),
       email: z.string(),
       date: z.string(),
     }),
     message: z.string(),
-    tree: z.object({ url: z.string(), sha: z.string() }),
-    comment_count: z.number(),
-    verification: z.object({
-      verified: z.boolean(),
-      reason: z.string(),
-      signature: z.string().nullable(),
-      payload: z.string().nullable(),
-    }),
   }),
-  author: z.object({
-    login: z.string(),
-    id: z.number(),
-    node_id: z.string(),
-    avatar_url: z.string(),
-    gravatar_id: z.string(),
-    url: z.string(),
-    html_url: z.string(),
-    followers_url: z.string(),
-    following_url: z.string(),
-    gists_url: z.string(),
-    starred_url: z.string(),
-    subscriptions_url: z.string(),
-    organizations_url: z.string(),
-    repos_url: z.string(),
-    events_url: z.string(),
-    received_events_url: z.string(),
-    type: z.string(),
-    site_admin: z.boolean(),
-  }),
-  committer: z.object({
-    login: z.string(),
-    id: z.number(),
-    node_id: z.string(),
-    avatar_url: z.string(),
-    gravatar_id: z.string(),
-    url: z.string(),
-    html_url: z.string(),
-    followers_url: z.string(),
-    following_url: z.string(),
-    gists_url: z.string(),
-    starred_url: z.string(),
-    subscriptions_url: z.string(),
-    organizations_url: z.string(),
-    repos_url: z.string(),
-    events_url: z.string(),
-    received_events_url: z.string(),
-    type: z.string(),
-    site_admin: z.boolean(),
-  }),
-  parents: z.array(z.object({ url: z.string(), sha: z.string() })),
 });
 
 const rateLimitSchema = z.object({
@@ -150,7 +92,7 @@ export const router = createTRPCRouter({
       const analyzedCommits = commits.map((commit) => ({
         ...commit,
         sentiment: sentiment.analyze(commit.commit.message),
-      }));  
+      }));
 
       return analyzedCommits;
     }),
