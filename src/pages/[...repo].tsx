811c8666b2af4d729e3426c438efa402ec218310
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-03-30 02:38:59 -0400

    adjust header styles

--- src/pages/[...repo].tsx
+++ src/pages/[...repo].tsx
@@ -21,13 +21,13 @@ const Repo = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="sticky top-0 z-50 border-b border-slate-800 bg-gray-50/80 backdrop-blur dark:bg-slate-900/80 ">
-        <div className="mx-auto flex max-w-6xl items-center justify-between gap-2 px-4 py-3">
-          <FileIcon className="h-5 w-5 text-slate-400" />
+        <div className="mx-auto flex max-w-4xl items-center justify-between gap-2 px-4 py-3">
+          <FileIcon className="h-4 w-4 text-slate-400" />
           <h1 className="min-w-0 flex-1 truncate font-semibold">{repo}</h1>
           <CommandPalette />
         </div>
       </header>
-      <main className="mx-auto max-w-6xl px-4 pb-4">
+      <main className="mx-auto max-w-4xl px-4 pb-4">
         <CommitTimeline repo={repo} />
       </main>
     </>
