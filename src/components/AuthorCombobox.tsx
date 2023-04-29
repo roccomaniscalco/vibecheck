fae62a60d87fbe60dc3a5b341e1b40783e1bb54e
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-04-28 23:58:31 -0400

    styling and accessability tweaks

--- src/components/AuthorCombobox.tsx
+++ src/components/AuthorCombobox.tsx
@@ -6,7 +6,7 @@ import {
   Cross1Icon,
   PersonIcon,
 } from "@radix-ui/react-icons";
-import { type MouseEvent, useState } from "react";
+import { type MouseEvent, useState, useRef } from "react";
 
 import { Button } from "@/components/ui/clickable";
 import {
@@ -33,8 +33,9 @@ type AuthorComboboxProps = {
 export function AuthorCombobox({
   repoFullName,
   author,
-  setAuthor,  
+  setAuthor,
 }: AuthorComboboxProps) {
+  const PopoverTriggerButtonRef = useRef<HTMLButtonElement | null>(null);
   const [open, setOpen] = useState(false);
 
   const authors = api.getCommits.useQuery(
@@ -61,6 +62,7 @@ export function AuthorCombobox({
 
   const handleClearAuthor = (event: MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
+    PopoverTriggerButtonRef.current?.focus();
     setAuthor("");
   };
 
@@ -71,6 +73,7 @@ export function AuthorCombobox({
       <div className="relative">
         <PopoverTrigger asChild>
           <Button
+            ref={PopoverTriggerButtonRef}
             role="combobox"
             aria-expanded={open}
             className="flex w-56 items-center gap-2 px-3 py-2"
@@ -87,10 +90,7 @@ export function AuthorCombobox({
                 {author}
               </>
             ) : (
-              <>
-                <PersonIcon className="h-4 w-4 opacity-50" />{" "}
-                {"Filter by author..."}
-              </>
+              <p className="pl-1">Filter by author...</p>
             )}
             {!author && (
               <CaretSortIcon className="ml-auto h-4 w-4 opacity-50" />
@@ -106,7 +106,7 @@ export function AuthorCombobox({
           </Button>
         )}
       </div>
-      <PopoverContent className="w-56 p-0" align="end">
+      <PopoverContent className="w-56 p-0" align="end" /*sideOffset={-36}*/>
         <Command>
           <CommandInput placeholder="Search author..." />
           <CommandEmpty>No author found.</CommandEmpty>
@@ -114,9 +114,10 @@ export function AuthorCombobox({
             {authors.data &&
               Object.entries(authors.data).map(([login, { avatar_url }]) => (
                 <CommandItem
+                  className="truncate"
                   key={login}
                   onSelect={() => {
-                    setAuthor(login === author.toLowerCase() ? "" : login);
+                    setAuthor(login === author ? "" : login);
                     setOpen(false);
                   }}
                 >
@@ -125,15 +126,15 @@ export function AuthorCombobox({
                   ) : avatar_url ? (
                     <Image
                       className="mr-2 block rounded-full"
-                      width={20}
-                      height={20}
+                      width={16}
+                      height={16}
                       src={`${avatar_url}?size=20`}
                       alt={`${author} avatar`}
                     />
                   ) : (
                     <PersonIcon className="h-4 w-4 opacity-50" />
                   )}
-                  {login}
+                  <p className="truncate">{login}</p>
                 </CommandItem>
               ))}
           </CommandGroup>
